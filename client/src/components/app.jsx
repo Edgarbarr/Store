9852781aebaedf2b85af78e1f2483c705398baf4
Edgar Barrientos <edgarbarrientos@Edgars-MacBook-Pro.local>
2020-05-03 10:00:12 -0700

    added stripe api connection

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+const axios = require('axios');
+
 
 function App(){
+    const [product, setProduct] = useState({
+        name: "Television",
+        price: 800,
+        country: "United States",
+        productImage: "https://www.ikea.com/us/en/images/products/brimnes-tv-bench__0850878_PE608594_S5.JPG"
+    })
+    const makePayment = token => {
+        const body = {
+            token,
+            product,
+        }
+        const headers = {
+            "Content-Type": "application/json"
+        }
+        axios.post("http://localhost:7777/payment", body)
+        .then(response => {
+            console.log("Response: ", response)
+            const {status} = response;
+            console.log("Status: ", status);
+        })
+        .catch(error => console.log(error));
+    }
     return (
-        <div>hi from react</div>
+        <div>
+            <div>
+                <span>{product.name}</span>
+                <img height="200" width="200" src={product.productImage}/>
+                <span>Price: {product.price}</span>
+                <StripeCheckout stripeKey="" token={makePayment} name="Checkout" amount={product.price * 100}>
+                    <button>Checkout</button>
+                </StripeCheckout>
+            </div>
+        </div>
     )
 }
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,53 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 7777;
 const path = require('path');
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STORE_SECRET_KEY);
+const {v4: uuidv4} = require('uuid');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.post("/payment", (req, res) => {
+
+    const { product, token } = req.body;
+    console.log("PRODUCT", product);
+    console.log("PRICE", product.price)
+    const idempotencyKey = uuidv4();
+
+    return stripe.customers
+    .create({
+        email: token.email,
+        source: token.id
+    })
+    .then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchase of ${product.name}.`,
+            // shipping: {
+            //     name: token.card.name,
+            //     address: {
+            //         country: token.card.address_country
+            //     }
+            // }
+        }, {idempotencyKey});
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => console.log(err))
+})
+// app.get("*", (req, res) => {
+//     res.send("hi from server");
+// })
 
 app.listen(port, ()=>{console.log(`Listening on port: ${port}`)});
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-    res.send("hi from server");
-})
